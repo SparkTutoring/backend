2dbb2f525dcce0cf1d2c427c7c8d0897c2b8b56c
Okkert <okkertjoub14@gmail.com>
2025-09-19 13:21:33 +0200

    Connected to a Database and added the Tutor Table

--- Tutors/models.py
+++ Tutors/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Tutors(models.Model):
+    """This is the Tutor Profiles Model"""
+    GENDER = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other')
+    ]
+    GROUP = [
+        ('Starter', 'Starter'),
+        ('Member', 'Member'),
+    ]
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10, unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    degree = models.CharField(max_length=50)
+    experience = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    group = models.CharField(max_length=50, choices=GROUP)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    calendarId = models.CharField(max_length=50)
+
+    class Meta:
+        """Meta class for Tutors Model"""
+        verbose_name = 'Tutor'
+        verbose_name_plural = 'Tutors'
+        indexes = [
+            models.Index(fields=['first_name', 'last_name']),
+            models.Index(fields=['phone']),
+            models.Index(fields=['group']),
+        ]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
