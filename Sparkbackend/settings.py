752a8da48db960f6e6f16ae96afe9e2b2205a8c6
Okkert <okkertjoub14@gmail.com>
2025-09-19 12:32:58 +0200

    Added Parents to the App

--- Sparkbackend/settings.py
+++ Sparkbackend/settings.py
@@ -37,12 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External Apps
+    'django_extensions',
 
     # Internal Apps
     'Tutors',
-
-    # External Apps
-    'django-extensions',
 ]
 
 MIDDLEWARE = [
