153942faba4504f55788f23a6d85498c9f227fb4
Okkert <okkertjoub14@gmail.com>
2025-09-19 12:26:23 +0200

    Added Tutors to the app

--- Sparkbackend/settings.py
+++ Sparkbackend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Internal Apps
+    'Tutors',
+
+    # External Apps
+    'django-extensions',
 ]
 
 MIDDLEWARE = [
